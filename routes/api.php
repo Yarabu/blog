6330b7049c74aaf7f97ddddf58c2281746f31d3a
Yarabu <yaroslava.butsychenko@oa.edu.ua>
2026-05-27 17:28:33 +0000

    feat: add admin categories

--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,15 @@ Route::group(['prefix' => 'blog'], function () {
     Route::apiResource('posts', PostController::class)->names('blog.posts');
 });
 
+$groupData = [
+    'namespace' => 'App\Http\Controllers\Blog\Admin',
+    'prefix' => 'admin/blog',
+];
+Route::group($groupData, function () {
+    //BlogCategory
+    $methods = ['index','store','update',];
+    Route::apiResource('categories', CategoryController::class)
+        ->only($methods)
+        ->names('blog.admin.categories');
+});
+
